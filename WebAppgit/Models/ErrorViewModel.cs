3c0b1f560003f328aa36893fe6de5c05f2e929d0
ammar <ammar@DELLG3-AM17>
2023-03-11 13:19:37 +0400

    Add project files.

---
+++ WebAppgit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppgit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
