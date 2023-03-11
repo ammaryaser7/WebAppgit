97b3c61b6e6b64ade6847c8e9e4a0bf6e12a84e7
ammar <ammar@DELLG3-AM17>
2023-03-11 13:59:23 +0400

    first commit

--- WebAppgit/Controllers/HomeController.cs
+++ WebAppgit/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebAppgit.Controllers
             return View();
         }
 
+        public IActionResult Filter()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
